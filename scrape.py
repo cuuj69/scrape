dbc97b0cd123521ecf400f9b9557c180afcef3b0
Dennis Dadey <deedadey@gmail.com>
2024-01-08 06:12:56 +0000

    fixed the divs for the various cars

--- scrape.py
+++ scrape.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
 dubizzle_base_url = 'https://uae.dubizzle.com/motors/used-cars'
+chrome_url = 'C:\Development\chromedriver.exe'
 
 # Get user's input and store them within variables
 city = ''
@@ -43,7 +46,8 @@ if filters[4] != '':
 print(dubizzle_url)
 
 # Start a new browser session (make sure to have the appropriate driver installed, e.g., chromedriver)
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
+driver = webdriver.Chrome(service=Service(executable_path=chrome_url))
 
 # Navigate to the URL
 driver.get(dubizzle_url)
@@ -56,7 +60,7 @@ page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
 # Find all divs that contain relevant information based on the presence of certain elements
-car_listing_divs = soup.find_all('div', {'type': 'primary'})
+car_listing_divs = soup.find_all('div', {'class': 'lpv-card-appear-done lpv-card-enter-done'})
 
 for car_div in car_listing_divs:
     # Extract specific information from each car_div
@@ -65,7 +69,7 @@ for car_div in car_listing_divs:
     kilometers_element = car_div.find('div', {'data-testid': 'listing-kms'})
     
     # Find the location element without specifying a class name
-    location_element = car_div.find('div')
+    location_element = '' #car_div.find('div')
 
     # Extract text content from the found elements
     price = price_element.get_text() if price_element else 'N/A'
